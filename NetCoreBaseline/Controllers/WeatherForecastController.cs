a92bdc1ddcde87e69400f73736fb9e8d34b73e44
divzi-p <38757978+divzi-p@users.noreply.github.com>
2022-08-17 12:29:48 +0530

    test change1

--- NetCoreBaseline/Controllers/WeatherForecastController.cs
+++ NetCoreBaseline/Controllers/WeatherForecastController.cs
@@ -32,7 +32,7 @@ namespace NetCoreBaseline.Controllers
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = rng.Next(-20, 55),
                 Summary = Summaries[rng.Next(Summaries.Length)],
-                Location="Asia"
+                Location="India"
             })
             .ToArray();
         }
